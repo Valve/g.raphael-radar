2acde273346c67bf0d1b273d1e9a2106f9e918f4
Valentin Vasilyev <iamvalentin@gmail.com>
2012-01-23 13:00:40 +0400

    Starting the work.

---
+++ README.md
@@ -0,0 +1,1 @@
+My version of a radar chart for Raphael.js
---
+++ g.radar.js
@@ -0,0 +1,10 @@
+/*
+ * g.raphael-radar 0.1 - Radar chart, based on g.Raphaël
+ *
+ * Copyright (c) 2012 Valentin Vasilyev (iamvalentin@gmail.com)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+ (function(paper, cx, cy, r, values, opts){
+ 	opts = opts || {};
+ 	var chart = paper.set(); 	
+ })();
