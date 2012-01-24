bef85b0580022c68891759755e8d95e437193b60
Valentin Vasilyev <iamvalentin@gmail.com>
2012-01-24 15:14:26 +0400

    Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-My version of a radar chart for Raphael.js
+# Raphaël radar chart
+
+## Radar chart implemented with [Raphael.js][raphael], version 0.1
+
+Works where Raphael.js works (Firefox 3.0+, Safari 3.0+, Chrome 5.0+, Opera 9.5+ and Internet Explorer 6.0+).
+
+## Basic usage
+
+``` 
+var paper = Raphael('some_div_id', 800, 800);
+paper.radar(cx, cy, radius, data_array, options);
+```
+
+To see real example, have a look at test/index.html 
+
+### Options
+
+This chart can be customized by passing the options that will override the default values, for example
+
+``` 
+paper.radar(400, 400, 300, [4, 8, 15, 16, 23, 42],
+  {
+    labels: ["USA", "Canada", "Sweden", "Ukraine", "Uzbekistan", "Russia"],
+    meshSize: 30,//the space between adjacent meshes
+    drawLabels: true, //to draw labels or not
+    armFill: 'none', //color of arm fill
+    armStroke: 'rgba(255, 106, 0, .5)', //color of arm stroke
+    armStrokeWidth: 1, //width of arm stroke
+    drawArms: true, //to draw arms or not
+    meshFill: 'none', //color of mesh fill
+    meshStroke: 'rgba(120, 120, 120, .5)', //color of mesh stroke
+    meshStrokeWidth: 1, //width of mesh stroke
+    drawMesh: true, //to draw mesh or not
+    max: 100, //maximum value, if not present, calculated from maximum value of data array
+    pathFill: 'none', //color of data path fill
+    pathStroke: '#0026ff', //color of data path stroke
+    pathStrokeWidth: 3, //data path stroke width
+    pathCircleOuterRadius: 4, //data path circle outer radius
+    pathCircleInnerRadius: 2, // data path circle inner radius
+    drawPathCircles: true //whether to draw the circles on the data path 
+  });
+```
+
+### Licence
+
+This code is [MIT][mit] licenced:
+
+Copyright (c) 2012 Valentin Vasilyev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+[raphael]: http://raphaeljs.com/
+[mit]: http://www.opensource.org/licenses/mit-license.php
--- g.radar.js
+++ g.radar.js
@@ -1,11 +1,165 @@
 /*
- * g.raphael-radar 0.1 - Radar chart, based on g.Raphaël
+ * g.raphael-radar 0.1 - Radar chart, based on Raphaël.js
  *
  * Copyright (c) 2012 Valentin Vasilyev (iamvalentin@gmail.com)
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
  */
- (function(paper, cx, cy, r, values, opts){
- 	opts = opts || {};
+ (function(){ 
+   function Radar(paper, cx, cy, r, values, opts){
+    if(!values || values.length == 0) throw 'Values array is required';
+   	opts = opts || {};
+    var 
+      startAngle = 270,
+      angle = 360/values.length;  
+    var defaultOpts = {
+      meshSize: 30,
+      labels: [],
+      drawLabels: true,
+      armFill: 'none',
+      armStroke: 'rgba(255, 106, 0, .5)',
+      armStrokeWidth: 1,
+      drawArms: true,
+      meshFill: 'none',
+      meshStroke: 'rgba(120, 120, 120, .5)',
+      meshStrokeWidth: 1,
+      drawMesh: true,
+      max: Math.max.apply(Math, values),
+      pathFill: 'none',
+      pathStroke: '#0026ff',
+      pathStrokeWidth: 3,
+      pathCircleOuterRadius: 4,
+      pathCircleInnerRadius: 2,
+      drawPathCircles: true   
+    };
+    //replacing default opts with explicitly provided
+    for(var prop in opts){
+      defaultOpts[prop] = opts[prop];    
+    }
+    opts = defaultOpts;
+    delete defaultOpts;
 
-  	
- })();
+    var arm = function(cx, cy, r, angle){
+      var rad = Raphael.rad(angle);
+      var x = cx + (r * Math.cos(rad));
+      var y = cy + (r * Math.sin(rad));
+      return ["M", cx, cy, "L", x, y, "Z"].join(",");
+    }
+
+    var meshLine = function(cx, cy, r, startAngle, angle){ 
+      var mesh = ["M"];
+      var circle = startAngle + 360;
+      while(startAngle < circle){
+        var x = cx + r * Math.cos(Raphael.rad(startAngle));
+        var y = cy + r * Math.sin(Raphael.rad(startAngle));
+        mesh.push(x);
+        mesh.push(y);
+        mesh.push("L");
+        startAngle += angle;
+      }
+      mesh.push("Z");
+      return mesh.join(",");       
+    }
+
+    var label = function(cx, cy, r, angle){
+      var rad = Raphael.rad(angle);
+      var x = cx + r * Math.cos(rad);
+      var y = cy + r * Math.sin(rad);
+      return {
+        x: (Math.round(x) === cx) ? x : (x < cx ? x - 10 : x + 10),
+        y: (Math.round(y) === cy) ? y : (y < cy ? y - 10 : y + 10),
+        attr:{
+          "text-anchor": (Math.round(x) === cx) ? "middle"  : (x < cx ?  "end" : "start"),
+          "font-size": r/20
+        }
+      };
+    }
+
+    var path = function(cx, cy, r, angle1, angle2, value1, value2, max){
+      var rad1 = Raphael.rad(angle1);
+      var rad2 = Raphael.rad(angle2);
+      var p1 = {
+        x: cx + r/max*value1 * Math.cos(rad1), 
+        y: cy + r/max*value1 * Math.sin(rad1)
+      };
+      var p2 = {
+        x: cx + r/max*value2 * Math.cos(rad2), 
+        y: cy + r/max*value2 * Math.sin(rad2)
+      };
+      paper.path(["M", p1.x, p1.y, "L", p2.x, p2.y, "Z"].join(",")).attr({
+        "stroke": opts.pathStroke,
+        "fill": opts.pathFill,
+        "stroke-width": opts.pathStrokeWidth
+      });     
+    }
+
+    var circle = function(cx, cy, r, angle, value, max, title){
+      var rad = Raphael.rad(angle);      
+      var p = {
+        x: cx + r/max*value * Math.cos(rad), 
+        y: cy + r/max*value * Math.sin(rad)
+      };   
+    
+      paper.circle(p.x, p.y, opts.pathCircleOuterRadius * 2).attr({fill: opts.pathStroke, stroke: 'none', title: title+": "+ value});        
+      paper.circle(p.x, p.y, opts.pathCircleInnerRadius * 2).attr({fill: "#fff", stroke: "none",title: title+": "+ value});    
+    }
+  
+    //arms
+    if(opts.drawArms){
+      for(i in values){    
+        paper.path(arm(cx, cy, r, startAngle + angle * i)).attr({
+          fill: opts.armFill,
+          stroke: opts.armStroke,
+          "stroke-width": opts.armStrokeWidth
+        });        
+      }
+    }
+    //mesh
+    if(opts.drawMesh){
+      var meshCount = Math.floor(r/opts.meshSize);
+      var meshHeight = r / meshCount;
+      var meshRadius = meshHeight;
+      var meshes = [];
+      while(meshCount > 0){
+        meshes.push(meshLine(cx, cy, meshRadius, startAngle, angle));
+        meshRadius+=meshHeight;
+        --meshCount;
+      }
+      for(i in meshes){
+        paper.path(meshes[i]).attr({
+          fill: opts.meshFill,
+          stroke: opts.meshStroke,
+          "stroke-width": opts.meshStrokeWidth 
+        });
+      }
+    }
+    //labels
+    if(opts.drawLabels){
+      for(i in opts.labels){
+        var textObject = label(cx, cy, r, startAngle + angle * i);
+        paper.text(textObject.x, textObject.y, opts.labels[i]).attr(textObject.attr);
+      }
+    }
+    
+    //path
+    for(var i = 0; i < values.length-1; i++){
+      path(cx, cy, r, startAngle + angle * i, startAngle + angle * (i + 1), values[i], values[i+1], opts.max);
+    } 
+    path(cx, cy, r, startAngle - angle, startAngle, values[values.length-1], values[0], opts.max);
+    
+    //circles on path
+    if(opts.drawPathCircles){
+      for(var i in values){
+        circle(cx, cy, r, startAngle + angle *i, values[i], opts.max, opts.labels[i]);
+      }
+    }    
+  }; 
+  //inheritance
+  var F = function() {};
+  F.prototype = Raphael.g;
+  Radar.prototype = new F;
+
+  //public
+  Raphael.fn.radar = function(cx, cy, r, values, opts) {
+      return new Radar(this, cx, cy, r, values, opts);
+  };  	
+})();
